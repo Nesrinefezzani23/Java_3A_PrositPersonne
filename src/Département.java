7f38efeff1e53bb603877143afe099eb929a8eb8
nesrine <nesrine.fezzani@esprit.tn>
2024-11-20 18:35:18 +0100

    Semaine 10: Set

---
+++ src/Département.java
@@ -0,0 +1,55 @@
+public class Département {
+    int id;
+    String nomDépartement;
+    int nbrEmployés;
+
+    public Département(){}
+    public Département(int id, String nomDépartement, int nbrEmployés) {
+        this.id = id;
+        this.nomDépartement = nomDépartement;
+        this.nbrEmployés = nbrEmployés;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNomDépartement() {
+        return nomDépartement;
+    }
+
+    public void setNom(String nom) {
+        this.nomDépartement = nomDépartement;
+    }
+
+    public int getNombreEmployes() {
+        return nbrEmployés;
+    }
+
+    public void setNombreEmployes(int nbrEmployés) {
+        this.nbrEmployés = nbrEmployés;
+    }
+
+    @Override
+    public String toString() {
+        return "Departement{" +
+                "id=" + id +
+                ", nom='" + nomDépartement + '\'' +
+                ", nombreEmployes=" + nbrEmployés +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (obj instanceof Département departement) {
+            return id == departement.id && nomDépartement.equals(departement.nomDépartement);
+        }
+        return false;
+    }
+
+}
